a1bbe183953f028330e64e8662fb132ae4618100
lionsoft <eleybov@gmail.com>
2013-12-26 10:58:19 +0200

    Init Commit

---
+++ CmdHidden/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdHidden
+{
+    // ReSharper disable PossibleMultipleEnumeration
+    class Program
+    {
+        public static void Main(params string[] args)
+        {
+            if (args.Length == 0) return;
+            IEnumerable<string> src = args;
+            if ((Path.GetExtension(args[0]) ?? "").ToLower() == ".exe")
+                src = src.Skip(1);
+            var scriptName = src.FirstOrDefault();
+            if (string.IsNullOrEmpty(scriptName)) return;
+            var argsStr = src.Skip(1).Aggregate("", (res, s) => res + " " + (s.Contains(" ") ? "\"" + s + "\"" : s)).Trim();
+            var psi = new ProcessStartInfo(scriptName, argsStr)
+            {
+                UseShellExecute = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            Process.Start(psi);
+        }
+    }
+}
